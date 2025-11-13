6a7d9d106c692f48f1a46bde5438a70508981292
kirandhakal <kirandhakal715@gmail.com>
2025-11-13 15:53:20 +0545

    common

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { ClassReading } from './ClassReading'
-import { SchoolReading } from './SchoolReading'
+// import { ClassReading } from './ClassReading'
+// import { SchoolReading } from './SchoolReading'
+import { PersonalReading } from './PersonalReading'
 import './App.css'
 
 function App() {
@@ -10,9 +11,12 @@ function App() {
       {/* <h1>Class reading form</h1>
       <ClassReading />
       <br /> <br /> */}
-      <div> <h1> School reading form</h1></div>
-      <SchoolReading />
+      {/* <div> <h1> School reading form</h1></div>
+      <SchoolReading /> */}
     
+<PersonalReading />
+
+   
       </div>
     </>
   )
---
+++ src/PersonalReading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Footer } from './components/Footer'
+import Header from './components/Header'
+// import { StudentTable } from './components/StudentTable'
+import { TitleSection } from './components/TitleSection'
+export const PersonalReading = () => {
+  return (
+   <>
+
+        <Header></Header>
+    <TitleSection />
+
+   
+    <Footer />
+   </>
+  )
+}
