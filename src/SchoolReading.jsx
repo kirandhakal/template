a09da513fbee8cb4ff00078e3df9b66922abcfb7
kirandhakal <kirandhakal715@gmail.com>
2025-11-16 16:07:14 +0545

    fix:A4

--- src/SchoolReading.jsx
+++ src/SchoolReading.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Footer } from './components/Footer'
-import Header from './components/Header'
-import { TitleSection } from './components/TitleSection'
-import DetaileFile from './components/DetaileFile'
-import TableComponents from './components/TableComponents'
-
+import React from "react";
+import { Footer } from "./components/Footer";
+import Header from "./components/Header";
+import { TitleSection } from "./components/TitleSection";
+import DetaileFile from "./components/DetaileFile";
+import TableComponents from "./components/TableComponents";
 
 export const SchoolReading = () => {
   return (
     <>
-    <div className="schoolReading">
-  <Header></Header>
-  <TitleSection Name={"SCHOOL"}/>
-  <DetaileFile></DetaileFile>
-    <TableComponents classes={["UKG ", "CLASS 1 ", "CLASS 3 ", "CLASS 4 "]} nepaliclasses={["युकेजी", "कक्षा १", "कक्षा २", "कक्षा ३"]} />
+      <div className="schoolReading">
+        <Header></Header>
+        <TitleSection Name={"SCHOOL"} />
+        <DetaileFile></DetaileFile>
+        <TableComponents
+          classes={["UKG ", "CLASS 1 ", "CLASS 3 ", "CLASS 4 "]}
+          nepaliclasses={["युकेजी", "कक्षा १", "कक्षा २", "कक्षा ३"]}
+        />
 
- {/* <TableComponents /> */}
-  <Footer />
-  </div>
+        {/* <TableComponents /> */}
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
