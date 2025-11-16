2e42ed53bb17d5e0de9e02f8a1619a3e18eb321c
kirandhakal <kirandhakal715@gmail.com>
2025-11-16 10:34:41 +0545

    title

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+// import "../assets/css/DetaileFile.css "
 
-export const TitleSection = () => {
+export const TitleSection = ({Name}) => {
   return (
    <>
      <div className="title">
           <img src="src/assets/logo1.webp" alt="logo1" />
-          <h1>CLASS READING PROFICIENCY REPORT</h1>
+          <h1>{Name} READING PROFICIENCY REPORT</h1>
           <img src="src/assets/logo1.webp" alt="logo2" />
         </div>
    </>
