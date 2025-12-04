afef5cbe9ab5ebf2ccf4cbd449e081fd3b221a87
kirandhakal <kirandhakal715@gmail.com>
2025-12-04 15:03:18 +0545

    all changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,20 @@ export const Footer = () => {
    <div className="footer">
        <div className="form-grid-3">
             <div className="form-field">
-               <p className="form-text">.........................</p>
+               <p className="form-text">.....................</p>
                {/* <p className="form-text">{'{assessedBy}'}</p> */}
               <label className="footer-form-label">Assessed by</label>
              
             </div>
             <div className="form-field">
               {/* <p className="form-text">{'{issuedDate}'}</p> */}
-              <p className="form-text">..........................</p>
+              <p className="form-text">.....................</p>
               <label className="footer-form-label">Issued Date</label>
               
             </div>
             <div className="form-field">
                {/* <p className="form-text">{'{executiveChairman}'}</p> */}
-                <p className="form-text">...........................</p>
+                <p className="form-text">.....................</p>
               <label className="footer-form-label">Executive Chairman</label>
              
             </div>
--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,74 +1,59 @@
 import "../assets/css/DetaileFile.css";
 
 export const StudentTable = () => {
+
+  const students = [
+    {
+      sn: 1,
+      name: "kiran dhakal",
+      english: ["A", "B", "A", "B", "A", "C"],
+      nepali: ["B", "A", "B", "C", "A", "B"]
+    },
+    {
+      sn: 2,
+      name: "ram Rai",
+      english: ["B", "A", "B", "C", "A", "A"],
+      nepali: ["A", "B", "A", "B", "B", "C"]
+    }
+  ];
+
   return (
     <>
       <div className="student-table-container">
         <table className="data-table">
           <thead>
             <tr>
-              <th rowSpan="2" className="th-sn">
-                S.N.
-              </th>
-              <th rowSpan="2" className="th-name">
-                Name of Students
-              </th>
-              <th colSpan="6" className="th-language">
-                ENGLISH
-              </th>
-              <th colSpan="6" className="th-language">
-                नेपाली
-              </th>
+              <th rowSpan="2" className="th-sn">S.N.</th>
+              <th rowSpan="2" className="th-name">Name of Students</th>
+              <th colSpan="6" className="th-language">ENGLISH</th>
+              <th colSpan="6" className="th-language">नेपाली</th>
             </tr>
+
             <tr>
               {["RSL", "PAUP", "IWC", "BCS", "UTC", "LRSF"].map((metric) => (
-                <th key={`eng-${metric}`} className="th-metric">
-                  {metric}
-                </th>
+                <th key={`eng-${metric}`}>{metric}</th>
               ))}
               {["RSL", "PAUP", "IWC", "BCS", "UTC", "LRSF"].map((metric) => (
-                <th key={`nep-${metric}`} className="th-metric">
-                  {metric}
-                </th>
+                <th key={`nep-${metric}`}>{metric}</th>
               ))}
             </tr>
           </thead>
-          <tbody>
-            {/* Placeholder for dynamically inserted rows  */}
-            {/* {'{studentsRows}'} */}
 
-            <tr>
-              <td>1.</td>
-              <td>John Doellllllllllllllllllllll</td>
-              <td>A</td>
-              <td>B</td>
-              <td>A</td>
-              <td>B</td>
-              <td>A</td>
-              <td>C</td>
-              <td>B</td>
-              <td>A</td>
-              <td>B</td>
-              <td>C</td>
-              <td>A</td>
-              <td>B</td>
-            </tr>
-            {/* <tr>
-              <td>2</td>
-              <td>John Doe</td>
-              <td>A</td>
-              <td>B</td>
-              <td>A</td>
-              <td>B</td>
-              <td>A</td>
-              <td>C</td>
-              <td>B</td>
-              <td>A</td>
-              <td>B</td>
-              <td>C</td>
-              <td>A</td>
-              <td>B</td>
-            </tr> */}
+          <tbody>
+            {students.map((std, index) => (
+              <tr key={index}>
+                <td>{std.sn}</td>
+                <td className="name-cell">{std.name}</td>
+
+                {std.english.map((mark, i) => (
+                  <td key={`eng-${index}-${i}`}>{mark}</td>
+                ))}
+
+                {std.nepali.map((mark, i) => (
+                  <td key={`nep-${index}-${i}`}>{mark}</td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
